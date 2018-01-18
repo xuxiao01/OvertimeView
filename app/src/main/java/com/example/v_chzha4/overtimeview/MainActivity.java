cbb6a4ea436aa004af2c7170f8a1b72c51d161fd
xuxiao01 <o-xuxiao01@beyondsoft.com>
2018-01-18 17:29:28 +0800

    count1

--- app/src/main/java/com/example/v_chzha4/overtimeview/MainActivity.java
+++ app/src/main/java/com/example/v_chzha4/overtimeview/MainActivity.java
@@ -1,5 +1,7 @@
+
 package com.example.v_chzha4.overtimeview;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -17,9 +19,12 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private DatePicker datePicker;
-    private int        year;
-    private int        month;
-    private int        day;
+    private int year;
+    private int month;
+    private int day;
+    float g1Count = 0;
+    float g2Count = 0;
+    float g3Count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +32,23 @@ public class MainActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        datePicker = (DatePicker)findViewById(R.id.datepicker);
+        datePicker = (DatePicker) findViewById(R.id.datepicker);
 
         initData();
         //countovertime();
     }
 
-    public void initData(){
+    public void initData() {
         Calendar calendar = Calendar.getInstance();
         year = calendar.get(Calendar.YEAR);
         month = calendar.get(Calendar.MONTH);
         day = calendar.get(Calendar.DAY_OF_MONTH);
 
-       datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
             @Override
             public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                 day = dayOfMonth;
-                addovertime(datePicker);
+                countovertime(datePicker);
                 //Toast.makeText(MainActivity.this, year + "年" + (monthOfYear+1) + "月" + dayOfMonth + "日", Toast.LENGTH_SHORT).show();
             }
         });
@@ -51,58 +56,77 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void enterWage(View v){
+    public void enterWage(View v) {
         Toast.makeText(this, "输入本月基本工资", Toast.LENGTH_SHORT).show();
         wageDialog(this);
     }
-    public void overtime(View v){
+
+    public void overtime(View v) {
 
         Toast.makeText(this, "加班小时数", Toast.LENGTH_SHORT).show();
     }
 
-    public static void wageDialog(Context context){
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context) ;
-        dialogBuilder.create() ;
-        LayoutInflater inflater = LayoutInflater.from(context) ;
-        View view = inflater.inflate(R.layout.wagelayout, null) ;
-        dialogBuilder.setView(view) ;
-        dialogBuilder.show() ;
+    public static void wageDialog(Context context) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+        dialogBuilder.create();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.wagelayout, null);
+        dialogBuilder.setView(view);
+        dialogBuilder.show();
     }
-    public void addovertime(View view){
+
+   /* public void addovertime(final View view) {
+
         LayoutInflater inflater = getLayoutInflater();
-        View view1= inflater.inflate(R.layout.dialog_layout,null);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this) ;
+        final View view1 = inflater.inflate(R.layout.dialog_layout, null);
+        //countovertime(view1);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         //builder.setTitle("添加工时");
         builder.setView(view1);
-        builder.create().show();
-    }
-    public void  countovertime() {
-        final RadioButton g1 = (RadioButton) findViewById(R.id.G1);
-        final RadioButton g2 = (RadioButton) findViewById(R.id.G2);
-        final RadioButton g3 = (RadioButton) findViewById(R.id.G3);
-        Button confime = (Button) findViewById(R.id.confime);
+        builder.create();
+        builder.show();
+        }*/
+
+    public void countovertime(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        final View view1 = inflater.inflate(R.layout.dialog_layout, null);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //builder.setTitle("添加工时");
+        builder.setView(view1);
+        builder.create();
+        final AlertDialog dialog = builder.show();
+        final RadioButton g1 = (RadioButton) view1.findViewById(R.id.G1);
+        final RadioButton g2 = (RadioButton) view1.findViewById(R.id.G2);
+        final RadioButton g3 = (RadioButton) view1.findViewById(R.id.G3);
+        Button confime = (Button) view1.findViewById(R.id.confime);
         confime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                float g1Count = 0;
-                float g2Count = 0;
-                float g3Count = 0;
-                EditText editText = (EditText) findViewById(R.id.edit1);
+
+                EditText editText = (EditText) view1.findViewById(R.id.edit1);
                 final float count = Float.valueOf(editText.getText().toString());
                 if (g1.isChecked()) {
                     g1Count += count;
                     String str1 = String.valueOf(g1Count);
                     Toast.makeText(MainActivity.this, str1, Toast.LENGTH_SHORT).show();
-                    System.out.println(g1Count);
+                    dialog.dismiss();
+                    //System.out.println(g1Count);
                 } else if (g2.isChecked()) {
                     g2Count += count;
-                    System.out.println(g2Count);
+                    String str2 = String.valueOf(g2Count);
+                    Toast.makeText(MainActivity.this, str2, Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                    //System.out.println(g2Count);
                 } else if (g3.isChecked()) {
                     g3Count += count;
-                    System.out.println(g3Count);
+                    String str3 = String.valueOf(g3Count);
+                    Toast.makeText(MainActivity.this, str3, Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                    //System.out.println(g3Count);
                 }
             }
         });
     }
 
 }
+
